4055b30b5c37ad679f36d27bcc406f219b90bf12
deepvr789 <dvrathod789@gmail.com>
2024-03-13 15:46:46 +0530

    datatypes

---
+++ Javascript/data_types.js
@@ -0,0 +1,20 @@
+var product_Name = "samsung galaxy S24";
+var brand = "Samsung";
+var prize = 130000;
+var product_Rating = 4.8;
+var color_Options = ["Black", "White"];
+var storage_Options = [128, 256, 500, 1];
+var isavail = true;
+var discount;
+
+
+//finding the type of data
+
+console .log(typeof product_Name); // string
+console .log(typeof brand); // object
+console .log(typeof prize); // number      
+console .log(typeof product_Rating); // number 
+console .log(typeof color_Options); // array
+console .log(typeof storage_Options); //
+console.log(typeof isavail); // boolean
+console.log(typeof discount);
