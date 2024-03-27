d579211a69631823935910973c6c20982efc70a0
deepvr789 <dvrathod789@gmail.com>
2024-03-27 11:45:43 +0530

    onblur

---
+++ Javascript/DOM/color.js
@@ -0,0 +1,18 @@
+
+
+function change_Color() {
+    document.getElementById('mobile').style.backgroundColor = "blue";
+}
+
+function change_Case() {
+    let usernameInput = document.getElementById("username");
+   
+    usernameInput.value =  usernameInput.value.toUpperCase();
+   
+  
+   
+}
+
+
+
+
