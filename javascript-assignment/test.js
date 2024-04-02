ae20e3db69325ae47d671df8109c02bd683733c0
deepvr789 <dvrathod789@gmail.com>
2024-04-02 09:26:20 +0530

    ok

--- javascript-assignment/test.js
+++ javascript-assignment/test.js
@@ -1,23 +1,9 @@
-function fibonacciSeries(limit) {
-    let n1 = 0, n2 = 1, nextTerm;
-    let series = '';
-
-    // Print the first two terms of the series
-    series += n1 + ' ' + n2 + ' ';
-
-    // Generate the Fibonacci series up to the given limit
-    nextTerm = n1 + n2;
-    while (nextTerm <= limit) {
-        series += nextTerm + ' ';
-        n1 = n2;
-        n2 = nextTerm;
-        nextTerm = n1 + n2;
-    }
-
-    console.log("Fibonacci Series up to " + limit + ":");
-    console.log(series);
+for (i=1; i<=10; i++){
+    if (i % 2 === 1) {
+        console.log(`${i} is odd`);
+        } else{
+            console.log(`${i} is even`);
+        }
+        
 }
-
-// Example: Print Fibonacci series up to 100
-fibonacciSeries(17);
 
