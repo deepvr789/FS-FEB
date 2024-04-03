d513386c485479e94dd7e1dc26f4cef85564363c
deepvr789 <dvrathod789@gmail.com>
2024-04-03 10:13:46 +0530

    tdt

--- javascript-assignment/test.js
+++ javascript-assignment/test.js
@@ -1,9 +1,11 @@
-for (i=1; i<=10; i++){
-    if (i % 2 === 1) {
-        console.log(`${i} is odd`);
-        } else{
-            console.log(`${i} is even`);
-        }
-        
-}
+//Write a program to print reverse of digits of numbers
+//Given number is 1234
 
+let num = 1234;
+let reversedNum = '';
+while (num != 0) {
+    let digit = num % 10;
+    reversedNum += digit;
+    num = Math.floor(num / 10);
+}
+console.log('Reverse of the given number: ' + reversedNum);
