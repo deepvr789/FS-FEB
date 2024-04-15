6da611af9513cbb055a5a13faa2d021f19fd5f9d
deepvr789 <dvrathod789@gmail.com>
2024-04-15 14:10:11 +0530

    15

---
+++ Javascript/es6/async/problemst/data.js
@@ -0,0 +1,21 @@
+let employees=[
+    {id:101,name:"Rahul Gandhi", sal:55000},
+    {id:102,name:"Sonia Gandhi", sal:65000},
+    {id:103,name:"Priyanka Gandhi", sal:75000},
+    {id:104,name:"Modi ", sal:85000},
+    {id:105,name:"Amith ", sal:95000},
+
+]
+let createEmployee
+let display_Employee(){
+    let rows=""
+    employees.forEach((employee)=>{
+        rows = rows + `<tr>
+                            <td>${employee.id}</td>
+                            <td>${employee.name}</td>
+                            <td>${employee.sal}</td>
+                       </tr>`
+    })
+    document.getElementById('abc').innerHTML =rows
+    
+}
---
+++ Javascript/es6/async/two.js
@@ -0,0 +1,17 @@
+setTimeout(()=> {
+    console.log("r1")
+
+},10000)
+
+setTimeout(()=> {
+    console.log("r2")
+
+},1000)
+setTimeout(()=> {
+    console.log("r3")
+
+},4000)
+setTimeout(()=> {
+    console.log("r4")
+
+},5000)
