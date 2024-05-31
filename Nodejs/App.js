a73bc5d509e16c7a235259dc47d4667d458b795c
deepvr789 <dvrathod789@gmail.com>
2024-05-31 12:51:35 +0530

    node http

--- Nodejs/App.js
+++ Nodejs/App.js
@@ -1,7 +1,28 @@
 //create server - http : node js core module
-const http = require('http');
+/*const http = require('http');
 const server = http.createServer((req,resp)=>{
     resp.end("hello,HTTP Server");
-
 });
-server.listen(8000, ()=>{})
+server.listen(8000, (err)=>{
+    if (err) throw err;
+    console.log("Server Running...");
+});*/
+
+/* const http = require('http');
+const server = http.createServer((req,resp)=>{
+    resp.end("hello,Node Server, nodemon!!!!");
+});
+server.listen(8000, (err)=>{
+    if (err) throw err;
+    console.log("server running");
+
+}); */
+
+/* const http = require("http")
+.createServer((req,resp)=> {
+resp.end(`<h1>Helo JS</h1>`)})
+.listen(7000, ()=> {
+    console.log("server running")
+}); */
+
+require("http").createServer((req,resp)=> {resp.end("Good Morning")}).listen(8000)
