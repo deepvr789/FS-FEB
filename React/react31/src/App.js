9d2410ce23105400df1e67d516720c6936e712f6
deepvr789 <dvrathod789@gmail.com>
2024-06-19 11:26:03 +0530

    test

--- React/react31/src/App.js
+++ React/react31/src/App.js
@@ -1,8 +1,9 @@
-import Message from "./Message"
+import Message from "./message"
 let App = ()=>{
 
     return <div>
             <h2>Welcome to React world</h2>
+            <h2>deepak rathod</h2>
             <hr />
             <Message/>
           
