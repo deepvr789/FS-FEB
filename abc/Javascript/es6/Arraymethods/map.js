dd15456b9f6c7f1f40378261a9e47865c94cadfc
deepvr789 <dvrathod789@gmail.com>
2024-07-25 18:24:32 +0530

    25

---
+++ abc/Javascript/es6/Arraymethods/map.js
@@ -0,0 +1,12 @@
+//using map add 1 to new prices
+let prices = [99,199,299,399,499]
+let newPrices = prices.forEach((price) => price + 1)
+console.log(prices)
+console.log(newPrices) 
+
+
+//using map convert the enames to uppercase
+let names = ['john','jane','jim','jill']
+let upperNames = names.map((name) => name.toUpperCase())
+console.log(names)
+console.log(upperNames)
