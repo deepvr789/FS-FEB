feed2774235091f432979706faf45afd314bb801
deepvr789 <dvrathod789@gmail.com>
2024-08-06 13:09:35 +0530

    React1st

---
+++ React/react5/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Movie from "./Movie/Movie";
+class App extends React.Component{
+
+render()  {
+        return <div>
+         <h1>App Component</h1>
+         <hr/>
+        <Movie/>
+        
+         </div>
+}    
+}
+export default App;
---
+++ React/react5/src/component/Slider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class Slider extends React.Component{
+
+
+render(){
+        return <div>
+            <h1>Slider Component</h1>
+
+
+
+        </div>
+    }
+}
+export default Slider;
